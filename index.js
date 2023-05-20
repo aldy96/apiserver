8f045f6c1b77a73a736a25c8fb3c503ff29ff5ac
Aldy Fernando Sobandi <47637558+aldy96@users.noreply.github.com>
2023-05-20 13:09:55 +0700

    new

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 
+app.use(cors({
+    origin:'*'
+}));
+
+
+app.get('/', (req, res) => {res.send({abc : '123'})})
+
+const port = 5000;
+app.listen(port);
